89e9dad7974ec8c7f1b86d54678c16d2ad17c70c
ivan <ivanov93@vk.com>
2022-07-16 10:37:10 +0300

    2.3. Введение в Maven и Spring Boot (Сделал ДЗ) внес исправления

--- src/main/java/comexamplecalculator/controller/FirstController.java
+++ src/main/java/comexamplecalculator/controller/FirstController.java
@@ -22,21 +22,24 @@ public class FirstController {
     }
 
     @GetMapping("/plus")
-    public String addition(@RequestParam("num1") String a, @RequestParam("num2") String b) {
-        return a + " + " + b + " = " + counterService.addition(Integer.parseInt(a), Integer.parseInt(b));
+    public String addition(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return a + " + " + b + " = " + counterService.addition(a, b);
     }
     @GetMapping("/minus")
-    public String subtraction(@RequestParam("num1") String a, @RequestParam("num2") String b) {
-        return a + " - " + b + " = " + counterService.subtraction(Integer.parseInt(a), Integer.parseInt(b));
+    public String subtraction(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return a + " - " + b + " = " + counterService.subtraction(a, b);
     }
 
     @GetMapping("/multiply")
-    public String multiplication(@RequestParam("num1") String a, @RequestParam("num2") String b) {
-        return a + " * " + b + " = " + counterService.multiplication(Integer.parseInt(a), Integer.parseInt(b));
+    public String multiplication(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return a + " * " + b + " = " + counterService.multiplication(a,b);
     }
     @GetMapping("/divide")
-    public String division(@RequestParam("num1") String a, @RequestParam("num2") String b) {
-        return a + " / " + b + " = " + counterService.division(Integer.parseInt(a), Integer.parseInt(b));
+    public String division(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        if (b == 0) {
+            return "Деление на 0 невозможно";
+        }
+        return a + " / " + b + " = " + counterService.division(a,b);
     }
 
 }
